eed56826273e8dde3dcc6a03774a1494a0a0abb7
RIKI SUPRIYATNA <rickysupriyatna@mhs.pelitabangsa.ac.id>
2022-10-30 22:49:47 +0700

    BISMILAH BISA

---
+++ app/src/main/java/com/wallpaperku/SplashScreenActivity.java
@@ -0,0 +1,34 @@
+package com.wallpaperku;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.View;
+
+public class SplashScreenActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+
+        // inisialisasi view
+        View decorView = getWindow().getDecorView();
+        // Hide the status bar.
+        decorView.setSystemUiVisibility(View.SYSTEM_UI_FLAG_FULLSCREEN);
+        // Hide ActionBar
+        if (getSupportActionBar() != null) {
+            getSupportActionBar().hide();
+        }
+        // timer
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(new Intent(SplashScreenActivity.this, MainActivity.class));
+                finish();
+            }
+        }, 3000);
+    }
+}
